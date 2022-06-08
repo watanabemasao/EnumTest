92cff005800c5b1c11e89c1d66450e2ba1e83dc1
渡辺正雄 <watanabemasao@watanabemasaonoMacBook-Pro.local>
2022-06-08 21:26:49 +0900

    enum記述修正、prodct内のswich文の戻り値をvoidに修正

--- src/main/java/product/Product.java
+++ src/main/java/product/Product.java
@@ -1,37 +1,31 @@
 package main.java.product;
 
-import org.jetbrains.annotations.Nullable;
-
-import static main.java.product.EnumFood.*;
-import static main.java.product.EnumDrink.*;
 public class Product {
     Drink drinkFind = Drink.COLA;
     Food foodFind = Food.POTATO;
 
-    private @Nullable EnumDrink.Drink drink() {
+    private void Drink() {
         switch (drinkFind) {
             case COLA -> System.out.println("コーラ");
             case ORANGE_JUICE -> System.out.println("グレープフルーツジュース" );
             case GRAPEFRUIT_JUICE -> System.out.println("オレンジジュース");
         }
-        return null;
-    }
-    public Drink getDrinkFind() {
-        return this.drink();
     }
 
+    public void getDrinkFind() {
+        this.Drink();
+    }
 
-    private @Nullable EnumFood.Food food() {
+    private void Food() {
             switch (foodFind) {
                 case POTATO -> System.out.println("ポテト");
                 case HOT_DOG -> System.out.println("ホットドッグ");
                 case SANDWICH -> System.out.println("サンドイッチ");
             }
-            return null;
         }
 
-    public Food getFoodFind() {
-        return this.food();
+    public void getFoodFind() {
+        this.Food();
     }
 
 }
